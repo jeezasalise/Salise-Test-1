97e6bf2206a0ea30696d9f00017e7609c07fa2cf
Jeeza Salise <j.salise.isi@gmail.com>
2019-10-31 00:03:58 +0800

    Removed redundant codes and faulty logic
    Deleted unnecessary method and calling of said method.  Also rejects '.txt' file extensions from being used to ensure that only supported file formats can be accepted.

--- assessment1.py
+++ assessment1.py
@@ -3,23 +3,19 @@ import json
 import csv
 from os import path
 
-def csvjsonf():
-    read_csvfile = sys.argv[1]
-    write_jsonfile = sys.argv[2]
-    if read_csvfile.endswith('.txt') or read_csvfile.endswith('.csv') and path.exists(read_csvfile):
-        if write_jsonfile.endswith('.txt') or write_jsonfile.endswith('.json'):
-            pass
-        else:
-            write_jsonfile += '.txt'
-            
-        data = {}
-        with open(read_csvfile, 'r') as csv_file:
-            csv_reader = csv.DictReader(csv_file, delimiter=',', skipinitialspace=True)
-            data = list(csv_reader)
-            with open(write_jsonfile, 'w') as json_file: # okay part
-                json_file.write(json.dumps(data, indent = 4))
-                print("Success!")
+read_csvfile = sys.argv[1]
+write_jsonfile = sys.argv[2]
+if read_csvfile.endswith('.csv') and path.exists(read_csvfile):
+    if write_jsonfile.endswith('.json'):
+        pass
     else:
-        print("Csv does not exist. Please check your input.\n")
-
-csvjsonf()
+        write_jsonfile += '.json'
+    data = {}
+    with open(read_csvfile, 'r') as csv_file:
+        csv_reader = csv.DictReader(csv_file, delimiter=',', skipinitialspace=True)
+        data = list(csv_reader)
+        with open(write_jsonfile, 'w') as json_file: # okay part
+            json_file.write(json.dumps(data, indent = 4))
+            print("Success!")
+else:
+    print("Csv file does not exist. Please check your input.\n")
