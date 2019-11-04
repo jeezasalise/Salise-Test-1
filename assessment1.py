74273b92afe4a3a3a644ed19b89486494b9f2101
Jeeza Salise <j.salise.isi@gmail.com>
2019-11-04 10:38:47 +0800

    Made it more user friendly
    It tells the user the required arguments if it has not been satisfied. It also shows an example of the accepted format.

--- assessment1.py
+++ assessment1.py
@@ -3,19 +3,24 @@ import json
 import csv
 from os import path
 
-read_csvfile = sys.argv[1]
-write_jsonfile = sys.argv[2]
-if read_csvfile.endswith('.csv') and path.exists(read_csvfile):
-    if write_jsonfile.endswith('.json'):
-        pass
+if len(sys.argv) > 2:
+
+    read_csvfile = sys.argv[1]
+    write_jsonfile = sys.argv[2]
+    if read_csvfile.endswith('.csv') and path.exists(read_csvfile):
+        if write_jsonfile.endswith('.json'):
+            pass
+        else:
+            write_jsonfile += '.json'
+        data = {}
+        with open(read_csvfile, 'r') as csv_file:
+            csv_reader = csv.DictReader(csv_file, delimiter=',', skipinitialspace=True)
+            data = list(csv_reader)
+            with open(write_jsonfile, 'w') as json_file:
+                json_file.write(json.dumps(data, indent = 4))
+                print("Success!")
     else:
-        write_jsonfile += '.json'
-    data = {}
-    with open(read_csvfile, 'r') as csv_file:
-        csv_reader = csv.DictReader(csv_file, delimiter=',', skipinitialspace=True)
-        data = list(csv_reader)
-        with open(write_jsonfile, 'w') as json_file: # okay part
-            json_file.write(json.dumps(data, indent = 4))
-            print("Success!")
+        print("Csv file does not exist. Please check the file name and file extension.\n")
+
 else:
-    print("Csv file does not exist. Please check the file name and file extension.\n")
+    print("Please follow the format: assessment1.py <csv_filename.csv> <json_filename.json> \nExample: assessment1.py csvtest.csv newfile.json")
